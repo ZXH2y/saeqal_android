7b11a8b1d78339f0e0928e57694b906a7deff34c
ZXH2y <andri9825559@gmail.com>
2026-07-08 22:35:08 +0800

    code untuk card hytam

--- app/src/main/java/com/example/saeqalfirdaus/ui/screen/ProfileScreen.kt
+++ app/src/main/java/com/example/saeqalfirdaus/ui/screen/ProfileScreen.kt
@@ -4,11 +4,15 @@ import com.example.saeqalfirdaus.ui.theme.SaeqalFirdausTheme
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 @Composable
@@ -20,15 +24,27 @@ fun ProfileScreen(){
                 brush = Brush.verticalGradient(
                     colors = listOf(
                         MaterialTheme.colorScheme.primaryContainer,
-                        MaterialTheme.colorScheme.primaryContainer,
+                        MaterialTheme.colorScheme.onSecondary,
                     )
                 )
             )
     ){
+// start code box hytam
         Box(
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxWidth()
+                .height(300.dp)
+                .background(
+                    brush = Brush.horizontalGradient(
+                        colors = listOf(
+                            MaterialTheme.colorScheme.primary,
+                            MaterialTheme.colorScheme.secondary,
+                        )
+                    ),
+                    shape = RoundedCornerShape(bottomStart = 10.dp, bottomEnd =20.dp)
+                )
         )
+// end code box hytam
     }
 }
 
